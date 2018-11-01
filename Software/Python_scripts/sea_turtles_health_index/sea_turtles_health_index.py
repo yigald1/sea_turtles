7982e1ddfaaf19a22fd99cfd3d738292a075d744
yigald <yigal.douchan@gmail.com>
2018-11-01 07:31:48 +0200

    Without standardization

--- Software/Python_scripts/sea_turtles_health_index/sea_turtles_health_index.py
+++ Software/Python_scripts/sea_turtles_health_index/sea_turtles_health_index.py
@@ -25,7 +25,7 @@ class Sea_turtles(object):
                             'total_time_spent']
         self._keys_pos = [0, 1, 4, 13, 14]     # position of keys in input record
         self._rechivim_dichotomic = [0, 0, 0, 0, 0]    # 1 for 0/1 rechiv (no standardization performed), 0 for regular numeric rechiv
-        self._rechivim_sign = [-1, -1, -1, -1, -1]     # injury severity, time spent, age, width, weight
+        self._rechivim_sign = [1, -1, -1, -1, -1]     # injury severity, time spent, age, width, weight
         self._weights_general = [100, 0, 0, 0, 0]  # weight by ccl_a_square
         self._rechiv_short_name = ['wt_ccla2', 'ccla_wt', 'scla_scw', 'ccla_ccw', 'ccw_wt']  # injury severity, ccla by weight, scla by scw, ccla by ccw, ccw by weight
         self._db_cursor = self._open_sea_turtles_db(self, db_name, user, password)
@@ -93,10 +93,11 @@ class Sea_turtles(object):
             line = []
             for field in self._keys_pos:
                 line.append(row[field])
-            validated_ccla_wt = self._validate_ccla_wt(self, line[3], line[4])
-            line.append(validated_ccla_wt)
+            calculated_madad = self._calculate_madad(self, line[3], line[4])
+            line.append(calculated_madad)
             sea_turtles_data.append(line)
         del sea_turtles_data[0]
+        sea_turtles_data_madad_assiron = self._calculate_assiron(self, sea_turtles_data)
 
         return sea_turtles_data
 
@@ -112,111 +113,132 @@ class Sea_turtles(object):
             return None
 
     @staticmethod
-    def _standardize_rechivim(self):
+    def _calculate_madad(self, weight, ccla):
 
-        _rechivim_val_pos = [41, 42, 43, 44, 45]  # position of validated rechivim in _sea_turtles_data
-        sea_turtles = []
-        curr_keys = []
-        keys_source = [[]]
-        curr_rechivim = []
-        rechivim_source = [[]]
-        curr_rechivim_for_standartization = []
-        rechivim_for_standartization = [[]]
-        rechivim_avg = []
-        rechivim_std = []
-        curr_standardized_rechivim = []
-        standardized_rechivim = [[]]
-
-        for st_idx, st in enumerate(self._sea_turtles_data):
-            for key in range(0, len(self._keys_pos)):
-                curr_keys.append(st[self._keys_pos[key]])
-            keys_source.append(copy.copy(curr_keys))
-            del curr_keys[:]
-        del keys_source[0]
-
-        for st_idx, st in enumerate(self._sea_turtles_data):
-            for rechiv in range(0, len(_rechivim_val_pos)):
-                curr_rechivim.append(st[_rechivim_val_pos[rechiv]])
-            rechivim_source.append(copy.copy(curr_rechivim))
-            del curr_rechivim[:]
-        del rechivim_source[0]
-
-        for rechiv_idx in range(0, len(_rechivim_val_pos)):
-            for st_idx, st in enumerate(rechivim_source):
-                if st[rechiv_idx] is not None:
-                    curr_rechivim_for_standartization.append(rechivim_source[st_idx][rechiv_idx])
-            rechivim_for_standartization.append(copy.copy(curr_rechivim_for_standartization))
-            del curr_rechivim_for_standartization[:]
-        del rechivim_for_standartization[0]
-
-        rechivim_avg = [statistics.mean(rechivim_for_standartization[i]) if self._rechivim_dichotomic[i] == 0 else 0 for i in range(len(self._rechivim_dichotomic))]
-        rechivim_std = [statistics.pstdev(rechivim_for_standartization[i]) if self._rechivim_dichotomic[i] == 0 else 0 for i in range(len(self._rechivim_dichotomic))]
-
-        for st_idx, st in enumerate(rechivim_source):
-            for rechiv_idx in range(0, len(_rechivim_val_pos)):
-                if self._rechivim_dichotomic[rechiv_idx] != 0:
-                    if rechivim_source[rechiv_idx] != 0:
-                        curr_standardized_rechivim.append(rechivim_source[st_idx][rechiv_idx])
-                    else:
-                        curr_standardized_rechivim.append(None)
-                else:
-                    if rechivim_source[st_idx][rechiv_idx] is None:
-                        curr_standardized_rechivim.append(None)
-                    else:
-                        curr_standardized_rechivim.append(abs((rechivim_source[st_idx][rechiv_idx] - rechivim_avg[rechiv_idx]) / rechivim_std[rechiv_idx]))
-                        # curr_standardized_rechivim.append((rechivim_source[st_idx][rechiv_idx] - rechivim_avg[rechiv_idx]) / rechivim_std[rechiv_idx])
-            standardized_rechivim.append(copy.copy(curr_standardized_rechivim))
-            del curr_standardized_rechivim[:]
-        del standardized_rechivim[0]
-
-        for key_idx, key in enumerate(keys_source):
-            sea_turtles.append(keys_source[key_idx] + rechivim_source[key_idx] + standardized_rechivim[key_idx])
-
-        return sea_turtles
-
-    def _prepare_madad_list(self, standardized_sea_turtles):
-        _sea_turtles_mdd = []
-        for sea_turtle in standardized_sea_turtles:
-            mdd_line = self.calculate_madad(sea_turtle)
-            _sea_turtles_mdd.append(mdd_line)
-        sea_turtles_mdd = self.calculate_assiron(_sea_turtles_mdd)
-
-        return sea_turtles_mdd
-
-    def calculate_madad(self, sea_turtle):
-        #  calculate the madad for each sea_turtle
-        _rechivim_std_pos = [17, 18, 19, 20, 21]  # position of standardized rechivim in sea_turtles at calculate_madad
-        standardized_rechivim_std_pos = []
-        for i in range(len(_rechivim_std_pos)):
-            standardized_rechivim_std_pos.append(_rechivim_std_pos[i])
-
-        weights_individual = copy.copy(self._weights_general)
-        weights_denominator = 0
-        madad = 0
-        rechivim = []
-
-        for rechiv in range(standardized_rechivim_std_pos[0], standardized_rechivim_std_pos[len(standardized_rechivim_std_pos) - 1] + 1):
-            rechivim.append(sea_turtle[rechiv])
-
-        # calculate individual weights for each sea_turtle (depending on missing values)
-        for i, rechiv in enumerate(rechivim):
-            if rechiv is None or (self._rechivim_dichotomic[i] == 1 and rechiv == 0):
-                weights_individual[i] = 0
+        ccla_lim = [14, 25, 35, 45, 55, 65, 75]
+        madad_ref = [1, 2, 4, 5, 6, 8, 9]
+        turtle_madad_ref = None
+        if type(ccla) == float and type(weight) == float:
+            if ccla > 0 and weight > 0:
+                for ccla_idx, ccla_step in enumerate(ccla_lim):
+                    if ccla <= ccla_step:
+                        turtle_madad_ref = madad_ref[ccla_idx]
+                        break
+                if turtle_madad_ref is None:
+                    turtle_madad_ref = 10
+                madad = (weight / ccla ** 2) / turtle_madad_ref
+                return madad
             else:
-                weights_individual[i] = self._weights_general[i]
-                weights_denominator += self._weights_general[i]
+                return None
+        else:
+            return None
 
-        # calculate madad based on individual weights
-        for i, rechiv in enumerate(rechivim):
-            if weights_individual[i] > 0:
-                madad += abs(rechivim[i] * self._rechivim_sign[i] * weights_individual[i] / weights_denominator)
-        sea_turtle.append(madad)
+    # @staticmethod
+    # def _standardize_rechivim(self):
+    #
+    #     _rechivim_val_pos = [41, 42, 43, 44, 45]  # position of validated rechivim in _sea_turtles_data
+    #     sea_turtles = []
+    #     curr_keys = []
+    #     keys_source = [[]]
+    #     curr_rechivim = []
+    #     rechivim_source = [[]]
+    #     curr_rechivim_for_standartization = []
+    #     rechivim_for_standartization = [[]]
+    #     rechivim_avg = []
+    #     rechivim_std = []
+    #     curr_standardized_rechivim = []
+    #     standardized_rechivim = [[]]
+    #
+    #     for st_idx, st in enumerate(self._sea_turtles_data):
+    #         for key in range(0, len(self._keys_pos)):
+    #             curr_keys.append(st[self._keys_pos[key]])
+    #         keys_source.append(copy.copy(curr_keys))
+    #         del curr_keys[:]
+    #     del keys_source[0]
+    #
+    #     for st_idx, st in enumerate(self._sea_turtles_data):
+    #         for rechiv in range(0, len(_rechivim_val_pos)):
+    #             curr_rechivim.append(st[_rechivim_val_pos[rechiv]])
+    #         rechivim_source.append(copy.copy(curr_rechivim))
+    #         del curr_rechivim[:]
+    #     del rechivim_source[0]
+    #
+    #     for rechiv_idx in range(0, len(_rechivim_val_pos)):
+    #         for st_idx, st in enumerate(rechivim_source):
+    #             if st[rechiv_idx] is not None:
+    #                 curr_rechivim_for_standartization.append(rechivim_source[st_idx][rechiv_idx])
+    #         rechivim_for_standartization.append(copy.copy(curr_rechivim_for_standartization))
+    #         del curr_rechivim_for_standartization[:]
+    #     del rechivim_for_standartization[0]
+    #
+    #     rechivim_avg = [statistics.mean(rechivim_for_standartization[i]) if self._rechivim_dichotomic[i] == 0 else 0 for i in range(len(self._rechivim_dichotomic))]
+    #     rechivim_std = [statistics.pstdev(rechivim_for_standartization[i]) if self._rechivim_dichotomic[i] == 0 else 0 for i in range(len(self._rechivim_dichotomic))]
+    #
+    #     for st_idx, st in enumerate(rechivim_source):
+    #         for rechiv_idx in range(0, len(_rechivim_val_pos)):
+    #             if self._rechivim_dichotomic[rechiv_idx] != 0:
+    #                 if rechivim_source[rechiv_idx] != 0:
+    #                     curr_standardized_rechivim.append(rechivim_source[st_idx][rechiv_idx])
+    #                 else:
+    #                     curr_standardized_rechivim.append(None)
+    #             else:
+    #                 if rechivim_source[st_idx][rechiv_idx] is None:
+    #                     curr_standardized_rechivim.append(None)
+    #                 else:
+    #                     curr_standardized_rechivim.append(abs((rechivim_source[st_idx][rechiv_idx] - rechivim_avg[rechiv_idx]) / rechivim_std[rechiv_idx]))
+    #                     # curr_standardized_rechivim.append((rechivim_source[st_idx][rechiv_idx] - rechivim_avg[rechiv_idx]) / rechivim_std[rechiv_idx])
+    #         standardized_rechivim.append(copy.copy(curr_standardized_rechivim))
+    #         del curr_standardized_rechivim[:]
+    #     del standardized_rechivim[0]
+    #
+    #     for key_idx, key in enumerate(keys_source):
+    #         sea_turtles.append(keys_source[key_idx] + rechivim_source[key_idx] + standardized_rechivim[key_idx])
+    #
+    #     return sea_turtles
+    #
+    # def _prepare_madad_list(self, standardized_sea_turtles):
+    #     _sea_turtles_mdd = []
+    #     for sea_turtle in standardized_sea_turtles:
+    #         mdd_line = self.calculate_madad(sea_turtle)
+    #         _sea_turtles_mdd.append(mdd_line)
+    #     sea_turtles_mdd = self.calculate_assiron(_sea_turtles_mdd)
+    #
+    #     return sea_turtles_mdd
 
-        return sea_turtle
+    # def calculate_madad(self, sea_turtle):
+    #     #  calculate the madad for each sea_turtle
+    #     _rechivim_std_pos = [17, 18, 19, 20, 21]  # position of standardized rechivim in sea_turtles at calculate_madad
+    #     standardized_rechivim_std_pos = []
+    #     for i in range(len(_rechivim_std_pos)):
+    #         standardized_rechivim_std_pos.append(_rechivim_std_pos[i])
+    #
+    #     weights_individual = copy.copy(self._weights_general)
+    #     weights_denominator = 0
+    #     madad = 0
+    #     rechivim = []
+    #
+    #     for rechiv in range(standardized_rechivim_std_pos[0], standardized_rechivim_std_pos[len(standardized_rechivim_std_pos) - 1] + 1):
+    #         rechivim.append(sea_turtle[rechiv])
+    #
+    #     # calculate individual weights for each sea_turtle (depending on missing values)
+    #     for i, rechiv in enumerate(rechivim):
+    #         if rechiv is None or (self._rechivim_dichotomic[i] == 1 and rechiv == 0):
+    #             weights_individual[i] = 0
+    #         else:
+    #             weights_individual[i] = self._weights_general[i]
+    #             weights_denominator += self._weights_general[i]
+    #
+    #     # calculate madad based on individual weights
+    #     for i, rechiv in enumerate(rechivim):
+    #         if weights_individual[i] > 0:
+    #             madad += abs(rechivim[i] * self._rechivim_sign[i] * weights_individual[i] / weights_denominator)
+    #     sea_turtle.append(madad)
+    #
+    #     return sea_turtle
 
-    def calculate_assiron(self, sea_turtles_mdd):
+    def calculate_assiron(self, sea_turtles_data):
 
-        sea_turtles_mdd.sort(key=lambda line: line[len(sea_turtles_mdd) - 1])
+        sea_turtles_data.sort(key=lambda line: line[len(sea_turtles_mdd) - 1])
         min_madad = sea_turtles_mdd[0][-1]
         _assiron_size = int(len(sea_turtles_mdd) / 10)
         for serialno, line in enumerate(sea_turtles_mdd):
